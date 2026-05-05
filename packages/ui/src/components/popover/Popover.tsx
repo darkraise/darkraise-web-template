743ed9af61177fad1282f4cccbf0975410c00349
Darkraise <quangtc94@gmail.com>
2026-05-06 00:17:38 +0700

    refactor(ui): migrate Popover to dr-popover class

--- packages/ui/src/components/popover/Popover.tsx
+++ packages/ui/src/components/popover/Popover.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import * as PopoverPrimitive from "@radix-ui/react-popover"
 
 import { cn } from "../../lib/utils"
+import "./popover.css"
 
 const Popover = PopoverPrimitive.Root
 
@@ -20,10 +21,7 @@ function PopoverContent({
         ref={ref}
         align={align}
         sideOffset={sideOffset}
-        className={cn(
-          "glass-strong bg-popover text-popover-foreground data-[state=closed]:animate-out data-[state=open]:animate-in data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 origin-[--radix-popover-content-transform-origin] border p-4 outline-none",
-          className,
-        )}
+        className={cn("dr-popover-content", className)}
         {...props}
       />
     </PopoverPrimitive.Portal>
