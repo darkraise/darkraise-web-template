ac73e6ba3f82e415d8c4b8f40232225c56ef6b08
Darkraise <quangtc94@gmail.com>
2026-05-05 23:55:43 +0700

    refactor(ui): migrate Skeleton to dr-skeleton class
    Moves Skeleton's styling into skeleton.css per the component class
    consolidation design.

--- packages/ui/src/components/skeleton/Skeleton.tsx
+++ packages/ui/src/components/skeleton/Skeleton.tsx
@@ -1,15 +1,11 @@
 import { cn } from "../../lib/utils"
+import "./skeleton.css"
 
 function Skeleton({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div
-      className={cn("bg-foreground/10 animate-pulse rounded-md", className)}
-      {...props}
-    />
-  )
+  return <div className={cn("dr-skeleton", className)} {...props} />
 }
 
 export { Skeleton }
